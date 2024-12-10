46a66a80c151a69080050e2437af1dae673de375
Yifung2002 <yifung0808@gmail.com>
2024-12-10 12:28:05 +0800

    Initial commit

--- update.php
+++ update.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    echo "Session not set. Redirecting to login page...";
+    header('Location: login.php');
+    exit();
+}
+
 // Database connection
 $conn = new mysqli('localhost', 'root', '', 'lab_5b');
 
@@ -10,7 +17,7 @@ if ($conn->connect_error) {
 if (isset($_GET['delete'])) {
     $matric = $_GET['delete'];
     $conn->query("DELETE FROM users WHERE matric = '$matric'");
-    header('Location: update.php'); 
+    header('Location: update.php');
     exit();
 }
 
@@ -32,7 +39,6 @@ $result = $conn->query($sql);
         <td><?php echo $row['role']; ?></td>
         <td>
             <a href="update_user.php?matric=<?php echo $row['matric']; ?>">Update</a>
-            |
             <a href="update.php?delete=<?php echo $row['matric']; ?>" onclick="return confirm('Are you sure you want to delete this user?')">Delete</a>
         </td>
     </tr>
